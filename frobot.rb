9eecd2a319c4f51d0efc7b133c333c742443b0c7
frantisek.gloser@gmail.com <frantisek.gloser@gmail.com>
2016-08-08 07:34:42 +0200

    after holiday

--- frobot.rb
+++ frobot.rb
@@ -5,9 +5,8 @@ class FlyingRobot < Robot
   def move (x, y, z)
     @current_place = [x, y, z] 
     @items = self.get_items(@current_place)
-    @items == nil || @items[0] == false ? puts("No weapon module here...") : @modules[0] = true && self.extend(WeaponModule)
-    @items == nil || @items[1] == false ? puts("No telecommunication module here...") : @modules[0] = true && self.extend(TelecommunicationModule)
-    puts @items.to_s + ' on place ' + @current_place.to_s
+    @items == nil || @items[0] == false ? puts("No weapon module here...") : @modules[0] = true && self.extend(WeaponModule) && puts('Yeah! Now I\'ve learned how to .shot.')
+    @items == nil || @items[1] == false ? puts("No telecommunication module here...") : @modules[1] = true && self.extend(TelecommunicationModule) && puts('Yeah! Now I\'ve learned how to .send_message_to_headquarters.')
     @current_place
   end
 
--- grobot.rb
+++ grobot.rb
@@ -6,12 +6,6 @@ require './weaponmodule'
 class Robot 
   attr_reader :current_place
 
-  def initialize (serial_number, x = 0, y = 0, z = 0)
-    @serial_number = serial_number
-    @current_place = [x, y, z]
-    @modules = [false, false]
-  end
-
   def identify
     @serial_number
   end
@@ -23,14 +17,27 @@ class Robot
 
   def install_items(items)
     items == nil || items[0] == false ? puts("No weapon module here...") : @modules[0] = true && self.extend(WeaponModule) && puts('Yeah! Now I\'ve learned how to .shot.')
-    items == nil || items[1] == false ? puts("No telecommunication module here...") : @modules[0] = true && self.extend(TelecommunicationModule) && puts('Yeah! Now I\'ve learned how to .send_message_to_headquarters.')
+    items == nil || items[1] == false ? puts("No telecommunication module here...") : @modules[1] = true && self.extend(TelecommunicationModule) && puts('Yeah! Now I\'ve learned how to .send_message_to_headquarters.')
   end
 
   def move (x, y, z)
-    @current_place[0] == x ? @current_place[1, 2] = [y, z]  : nil 
-    @items = self.get_items(@current_place)
-    install_items(@items)
+    if @current_place[0] == x  
+      @current_place[1, 2] = [y, z]
+      @items = self.get_items(@current_place)
+      install_items(@items)
+    else
+      puts("Wheel robots cannot fly!")
+    end
     @current_place
   end
+
+  private
+
+  def initialize (serial_number, x = 0, y = 0, z = 0)
+    @serial_number = serial_number
+    @current_place = [x, y, z]
+    @modules = [false, false]
+  end
+
 
 end  
